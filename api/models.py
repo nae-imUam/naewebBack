a8cc4fca37d27c20c2bef54b9be9a9866c69b1e8
nae-imUam <nae.imuam103@gmail.com>
2024-01-22 11:16:39 +0530

    added quiz_data to userdata

--- api/models.py
+++ api/models.py
@@ -16,16 +16,19 @@ class UserData(models.Model):
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=128) 
-
+    quiz_data = models.JSONField(default=list) 
+    
     def __str__(self):
         return self.username
+
 
 
 class MiniTest(models.Model):
     test_number = models.PositiveIntegerField(unique=True)
     class_name = models.CharField(max_length=50)
     subject = models.CharField(max_length=50)
+    chapter = models.CharField(max_length=100, default='Do Your Best')
     questions = models.JSONField()
 
     def __str__(self):
-        return f"MiniTest {self.test_number} - {self.class_name} - {self.subject}"
+        return f"MiniTest {self.test_number} - {self.class_name} - {self.subject} - {self.chapter}"
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import signup, login_view
 from rest_framework.routers import DefaultRouter
-from .views import MiniTestViewSet, check_username_availability,  get_minitest_by_test_number
+from .views import MiniTestViewSet, check_username_availability,  get_minitest_by_test_number, MiniTestInfoListView, QuizDataUpdateView, QuizDataRetrieveView
 
 router = DefaultRouter()
 router.register(r'minitests', MiniTestViewSet)
@@ -11,7 +11,10 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('check-username/<str:username>/', check_username_availability, name='check_username_availability'),
     path('minitests/<int:test_number>/', get_minitest_by_test_number, name='get_minitest_by_test_number'),
+    path('minitests/<str:class_name>/<str:subject>/', MiniTestInfoListView.as_view(), name='mini-test-list'),
     path('', include(router.urls)),
+    path('userdata/<str:username>/update_quiz_data/', QuizDataUpdateView.as_view(), name='update_quiz_data'),
+    path('userdata/<str:username>/quiz_data/', QuizDataRetrieveView.as_view(), name='quiz_data'),
     # Add more URL patterns as needed
 ]
 
